544aa550e6405c6ee3f004f28007b03ed82cdf79
Ethan Winegardner <ejosephhiii@gmail.com>
2022-07-25 12:49:03 -0600

    update

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-let Rollbar = require("rollbar");
-let rollbar = new Rollbar({
-  accessToken: 'b18a0032b8f7437eb56d39e245024298',
-  captureUncaught: true,
-  captureUnhandledRejections: true
-});
-
 
 let button = document.querySelector("#buy")
+
 let buttonTwo = document.querySelector("#disclaimer")
+
 buyButton = () => {
-    rollbar.log("user tried to buy the sword")
     alert("Siiiiike you don't got the power for this sweet baby! LOL")
 }
 
 disclaimerButton = () => {
-    rollbar.log('lol the user got trolled')
     alert("I am not responsible for any broken personal objects for trying to buy LOL")
 }
 
-
+// rollbar.log("frontend hit")
 
 button.addEventListener('click', buyButton)
 buttonTwo.addEventListener('click', disclaimerButton)
